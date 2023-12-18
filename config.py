5c212fba79c50bd6704a1a58be40796e6a743db3
Madara Shelby <140615675+MadaraShelby@users.noreply.github.com>
2023-12-19 01:31:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 from pyrogram import filters
 
 class Config:
-    API_ID = "20136640"
-    API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
-    TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    API_ID = "27202551"
+    API_HASH = "e070ca5c168293a0a4b3f19e3eeb2855"
+    TOKEN = "6647266805:AAH1uzqzfYoFI8jYUrlPSSOry1a3tuNXajQ"
     MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
     SUDOERS = filters.user(["6691393517"])
